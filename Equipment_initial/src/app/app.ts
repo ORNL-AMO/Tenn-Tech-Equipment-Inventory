a0762fded8f888047d5d2a09ba3bb807b9f9d845
Avantika Laine <dnlaine42@tntech.edu>
2025-12-04 16:38:27 -0600

    add new component, upload-image

--- Equipment_initial/src/app/app.ts
+++ Equipment_initial/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { UploadImage } from './upload-image/upload-image';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, UploadImage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
