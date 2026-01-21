812f35ce75c1195d37c6877f328f250af09ed859
Avantika Laine <dnlaine42@tntech.edu>
2026-01-21 14:08:53 -0600

    working on webcam support

--- Equipment_initial/src/app/upload-image/upload-image.ts
+++ Equipment_initial/src/app/upload-image/upload-image.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { isNgContainer } from '../../../node_modules/@angular/compiler/types/compiler';
-import * as extraction from "../../scripts/extraction";
+// import * as extraction from "../../scripts/extraction";
 
 @Component({
   selector: 'app-upload-image',
@@ -9,7 +9,7 @@ import * as extraction from "../../scripts/extraction";
   styleUrl: './upload-image.css'
 })
 export class UploadImage {
-  ngOnInit(): void {
-    extraction.processImage()
-  }
+  // ngOnInit(): void {
+  //   extraction.processImage()
+  // }
 }
