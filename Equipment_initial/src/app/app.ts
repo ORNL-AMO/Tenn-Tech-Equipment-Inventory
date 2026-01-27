a9ebfc534ff74ff40060dfff405afc3331b681fe
Avantika Laine <dnlaine42@tntech.edu>
2026-01-27 15:33:55 -0600

    removing wgx-webcam functionality

--- Equipment_initial/src/app/app.ts
+++ Equipment_initial/src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { WebcamImage, WebcamModule } from 'ngx-webcam';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { UploadImage } from './upload-image/upload-image';
@@ -7,25 +6,10 @@ import { UploadImage } from './upload-image/upload-image';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, UploadImage, WebcamModule],
+  imports: [RouterOutlet, Header, UploadImage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('MEASUR Proof of Concept');
-  public webcamImage: WebcamImage = null;
-  private trigger: Subject<void> = new Subject<void>();
-
-  triggerSnapshot(): void {
-    this.trigger.next();
-  }
-
-  handleImage(webcamImage: WebcamImage): void {
-    console.info('Captured image:', webcamImage);
-    this.webcamImage = webcamImage;
-  }
-
-  get triggerObservable(): Observable<void> {
-    return this.trigger.asObservable();
-  }
 }
--- Equipment_initial/src/app/upload-image/upload-image.ts
+++ Equipment_initial/src/app/upload-image/upload-image.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { isNgContainer } from '../../../node_modules/@angular/compiler/types/compiler';
-// import * as extraction from "../../scripts/extraction";
+import * as extraction from "../../scripts/extraction";
 
 @Component({
   selector: 'app-upload-image',
@@ -8,8 +7,9 @@ import { isNgContainer } from '../../../node_modules/@angular/compiler/types/com
   templateUrl: './upload-image.html',
   styleUrl: './upload-image.css'
 })
+
 export class UploadImage {
-  // ngOnInit(): void {
-  //   extraction.processImage()
-  // }
+  processImg() {
+    extraction.processImage()
+  }
 }
