3c614add42d78843d010d55ac05c28cbe3a8eaf2
Avantika Laine <dnlaine42@tntech.edu>
2026-01-29 12:29:51 -0600

    Got computer webcam component fully functional (except switching device)

--- Equipment_initial/src/app/webcam/webcam.ts
+++ Equipment_initial/src/app/webcam/webcam.ts
@@ -9,53 +9,62 @@ import { CommonModule } from '@angular/common';
 })
 export class Webcam {
   deviceId?: string;
+  currentDevice?: string;
   showDevices: boolean = false;
   webcamPermission: boolean = false;
   camOn: boolean = false;
-  
+
+  //Call these functions via buttons on the HTML page
   checkPermission() {
     this.requestCameraPermissions();
     this.setMediaDevices();
+    this.webcamPermission = true;
   }
   chooseDevice() {
     this.setMediaDevices();
     this.showDevices = !this.showDevices;
   }
   selectDevice(device: MediaDeviceInfo) {
-    this.deviceId=device.deviceId;
+    this.deviceId = device.deviceId;
     this.showDevices = false;
-    if(this.camOn){
-      this.startCam();
-    }
+    if (this.camOn) {this.startCam();}
+    console.log(this.deviceId);
+    this.currentDevice = device.label;
+    console.log(this.currentDevice);
   }
   startCam() {
     this.startStreaming();
     this.camOn = true;
   }
   stopCam() {
-    this.closeStream(this.mediaStream);
+    this.closeStream()
+    this.camOn = false;
   }
 
 
   async requestCameraPermissions() {
-    const constraints = { video: true, audio: false };
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    let tracks: Array<MediaStreamTrack> = stream.getTracks();
-    for (let i = 0; i < tracks.length; i++) {
-      const track = tracks[i];
-      track.stop();
+    try {
+      console.log("Camera Permission request");
+      const constraints = { video: true, audio: false };
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      // let tracks: Array<MediaStreamTrack> = stream.getTracks();
+      // for (let i = 0; i < tracks.length; i++) {
+      //   const track = tracks[i];
+      //   track.stop();
+      // }
+      stream.getTracks().forEach(track => track.stop());
+      console.log("Camera permission granted");
+    } catch (err) {
+      console.log("Camera Permission error");
+      console.log(err);
+      this.webcamPermission = false;
+      alert("Camera Permission Denied\nPlease grant camera permission to this website to use camera functionality");
     }
   }
 
   mediaDevices: Array<MediaDeviceInfo> = []
   async setMediaDevices() {
     let allDevices = await navigator.mediaDevices.enumerateDevices();
-    if(allDevices.length>2){
-      this.webcamPermission = true;
-    }
-    else{
-      this.webcamPermission = false;
-    }
     this.mediaDevices = new Array();
     for (let i = 0; i < allDevices.length; i++) {
       let device = allDevices[i];
@@ -63,7 +72,6 @@ export class Webcam {
         this.mediaDevices.push(device);
       }
     }
-  
   }
 
   mediaStream!: MediaStream;
@@ -77,14 +85,13 @@ export class Webcam {
     /*<video id="videoElement" #videoElement />*/
     this.videoElement.nativeElement.srcObject = this.mediaStream;
     this.videoElement.nativeElement.play();
+    console.log("Camera Started");
   }
 
-  closeStream(stream: MediaStream) {
-    let tracks: Array<MediaStreamTrack> = stream.getTracks();
-    for (let i = 0; i < tracks.length; i++) {
-      const track = tracks[i];
-      track.stop;
-    }
+  closeStream(stream: MediaStream = this.mediaStream) {
+    stream.getTracks().forEach(track => track.stop());
+    this.videoElement.nativeElement.srcObject = null;
+    console.log("Camera Closed");
   }
 
   async getSelectedDeviceMediaStream(): Promise<MediaStream> {
