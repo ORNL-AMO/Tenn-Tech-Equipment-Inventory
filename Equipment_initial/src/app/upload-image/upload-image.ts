05e9eaf52aa0df0544feab02e021d644eab3a929
Avantika Laine <dnlaine42@tntech.edu>
2026-02-27 13:47:37 -0600

    Updated the Example popup to use Material a card

--- Equipment_initial/src/app/upload-image/upload-image.ts
+++ Equipment_initial/src/app/upload-image/upload-image.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { OCRComponent } from '../ocr/ocr.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-upload-image',
-  imports: [OCRComponent],
+  imports: [OCRComponent, MatCardModule],
   templateUrl: './upload-image.html',
-  styleUrl: './upload-image.css'
+  styleUrl: './upload-image.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class UploadImage {
