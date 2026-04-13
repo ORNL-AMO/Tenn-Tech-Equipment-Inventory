d8bfe163d9a52d62316eff1599612197ac5c2b64
Avantika Laine <dnlaine42@tntech.edu>
2026-04-13 13:27:36 -0500

    moved help section into app

--- Equipment_initial/src/app/app.ts
+++ Equipment_initial/src/app/app.ts
@@ -4,11 +4,12 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { Header } from './header/header';
 import { History } from './history/history';
 import { OCRComponent } from './ocr/ocr.component';
+import { HelpButton } from './help-button/help-button';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, OCRComponent, MatTabsModule, Header, History],
+  imports: [RouterOutlet, OCRComponent, MatTabsModule, Header, History, HelpButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
